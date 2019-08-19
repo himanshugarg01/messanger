db68955458a8b12bcab22e7b13b4b179a0b47037
himanshugarg01 <himanshu171321.cse@chitkara.edu.in>
2019-08-19 17:59:38 +0530

    improve frontend

--- model/messages.js
+++ model/messages.js
@@ -3,6 +3,8 @@ const mongoose = require('./db');
 let messages = new mongoose.Schema({
     users : Array,
     message : Array
+},{
+  timestamps: true,
 });
 
 module.exports = mongoose.model('messages', messages);
